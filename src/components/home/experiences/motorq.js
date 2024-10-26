8a1c6a839447926909485200a666b8975045f3d4
Siddhant Jain <siddhant.jain120@gmail.com>
2024-10-27 03:42:50 +0530

    update experience - minor

--- src/components/home/experiences/motorq.js
+++ src/components/home/experiences/motorq.js
@@ -2,7 +2,7 @@ const motorq = {
     title: "Software Engineer Intern",
     company: "Motorq",
     period: "May 2023 - July 2023",
-    description: "Moved caching from in-memory to a shared redis cluster. Also, built a developer tool on Azure."
+    description: "Worked on moving caching from in-memory to a shared redis cluster. Also, built a developer tool to automate housekeeping tasks on azure."
   };
   
   export default motorq;
--- src/components/home/experiences/traceable.js
+++ src/components/home/experiences/traceable.js
@@ -2,7 +2,7 @@ const traceable = {
     title: "Software Engineer",
     company: "Traceable",
     period: "July 2024 - Present",
-    description: "Part of the platform team."
+    description: "Part of the platform team. Building scalable and reliable systems to process and analyze large amounts of data.",
   };
   
   export default traceable;
