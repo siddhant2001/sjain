027620b27ce554360eac8bca5529ad66b8b6f282
Siddhant Jain <siddhant.jain120@gmail.com>
2024-10-27 03:47:35 +0530

    minor fixes

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -33,12 +33,14 @@ const Navbar = () => {
   return (
     <nav className={scrolled ? "navbar scrolled" : "navbar"}>
       <a href={logoLink} className="nav-logo">{logoText}</a>
+      {isBlogPage ? (<div id="blank"></div>) : 
       <ul className={isMobile ? "nav-links-mobile" : "nav-links"}>
         <li><a href="#about" onClick={() => setIsMobile(false)}>About</a></li>
         <li><a href="#experience" onClick={() => setIsMobile(false)}>Experience</a></li>
         <li><a href="#publications" onClick={() => setIsMobile(false)}>Publications</a></li>
         <li><a href="#education" onClick={() => setIsMobile(false)}>Education</a></li>
       </ul>
+      }
       <button
         className="mobile-menu-icon"
         onClick={() => setIsMobile(!isMobile)}
